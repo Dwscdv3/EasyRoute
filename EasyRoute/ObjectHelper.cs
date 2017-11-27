5e28d0741a047910ace19188504f7fb3004235e9
Dwscdv3 Konno <dwscdv3@hotmail.com>
2017-11-27 23:58:19 +0800

    change namespace

--- EasyRoute/ObjectHelper.cs
+++ EasyRoute/ObjectHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using static EasyRoute.Utils;
 
-namespace EasyRoute.Extension
+namespace EasyRoute.Extensions
 {
     public static class ObjectHelper
     {
